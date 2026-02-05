a302ee8e42a9d4f158dee861b1584ae8d282fb3e
ahmedtharwat-at <ahmedsarwat3000@gmail.com>
2026-02-05 14:20:01 +0200

    added free condition for the pricing plan

--- src/components/PricingPlanCard.tsx
+++ src/components/PricingPlanCard.tsx
@@ -165,16 +165,22 @@ export default function PricingPlanCard({
               "border-accent-800": isFeatured,
             })}
           >
-            <span className="text-3xl text-secondary font-bold">
-              {priceFormatter.format(price)}
-            </span>
-            <span
-              className={cn("text-lg text-gray-dark", {
-                "text-white": isFeatured || isDemo,
-              })}
-            >
-              {type === "yearly" ? "جنيه /سنة" : "جنيه /شهر"}
-            </span>
+            {price > 0 ? (
+              <>
+                <span className="text-3xl text-secondary font-bold">
+                  {priceFormatter.format(price)}
+                </span>
+                <span
+                  className={cn("text-lg text-gray-dark", {
+                    "text-white": isFeatured || isDemo,
+                  })}
+                >
+                  {type === "yearly" ? "جنيه /سنة" : "جنيه /شهر"}
+                </span>
+              </>
+            ) : (
+              <span className="text-3xl text-secondary font-bold">مجاني</span>
+            )}
           </div>
 
           {!isOverview && (
