2ea72787e8e26946b8a93fbc99feed9746592bc7
ahmedtharwat-at <ahmedsarwat3000@gmail.com>
2026-02-11 16:05:42 +0200

    extend dialog to allow porps passing on auto close

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -47,11 +47,12 @@ const DialogContent = React.forwardRef<
         "data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed top-[50%] left-[50%] z-100 grid max-h-[calc(100dvh-20px)] w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 overflow-y-auto border border-neutral-200 bg-white p-6 shadow-lg duration-200 sm:rounded-lg dark:border-neutral-800 dark:bg-neutral-950",
         className,
       )}
+      {...props}
       onCloseAutoFocus={(event) => {
         event.preventDefault();
         document.body.style.pointerEvents = "auto";
+        props?.onCloseAutoFocus?.(event);
       }}
-      {...props}
     >
       {children}
       {hideClose === false && (
